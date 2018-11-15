44f6c18d31ee87ee4bfcc8297a9ad649a6e81226
RasmusChristensen <rasmus@christensen.io>
2018-11-15 16:35:52 +0100

    Moved lines.

--- UniMeetUp/CommonLib/Models/ChatMessage.cs
+++ UniMeetUp/CommonLib/Models/ChatMessage.cs
@@ -23,8 +23,8 @@ namespace CommonLib.Models
 
 
         public string UserId { get; set; }
-        public int GroupId { get; set; }
         public User User { get; set; }
+        public int GroupId { get; set; }
         public Group Group { get; set; }
     }
 }
